0f6b9acc307a87eb351f3e458a4a19f80034d3ff
Divyajoshi8 <97658852+Divyajoshi8@users.noreply.github.com>
2026-04-06 20:20:28 +0530

    models

---
+++ models/expenseModel.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+
+
+
+
+module.exports = Expense;
+
+
+
+
+
+
+
+// const sequelize = require('sequelize');
+// const db = require('../utils/database');
+
+// const Expenses = db.define("expenses", {
+//     id:{
+//         type:sequelize.INTEGER,
+//         autoIncrement: true,
+//         allowNull: false,
+//         primaryKey: true,
+//     },
+//     date:{
+//         type:sequelize.STRING,
+//         allowNull:true,
+//     },
+//     category:{
+//         type:sequelize.STRING,
+//         allowNull:true,
+//     },
+//     description:{
+//         type:sequelize.STRING,
+//         allowNull:true,
+//     },
+//     amount:{
+//         type:sequelize.INTEGER,
+//         allowNull:true,
+//     },
+// });
+
---
+++ models/forgotpasswordrequests.js
@@ -0,0 +1,38 @@
+// const Sequelize = require('sequelize');
+// const sequelize = require('../utils/database');
+
+// const ForgotPasswordRequests = sequelize.define("forgotpasswordrequests", {
+//     uuid:{
+//         type:Sequelize.STRING,
+//       },
+//     userId:{
+//         type:Sequelize.INTEGER,
+//     },
+//     isactive:{
+//         type:Sequelize.BOOLEAN,
+//     }
+
+const mongoose = require("mongoose");
+
+const ForgotPasswordRequests = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+
+const ResetPassword = mongoose.model("ResetPassword", ForgotPasswordRequests);
+
+module.exports = ResetPassword;
+// module.exports = ForgotPasswordRequests;
